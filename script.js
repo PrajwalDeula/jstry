b2805f29a3088236b54eacbe3d5baeda077b7dc6
Prajwal Deula <prajwaldeula@gmail.com>
2023-10-07 22:42:19 +0545

    this is right

---
+++ script.js
@@ -0,0 +1,10 @@
+class Car{
+	constructor(name,year){
+		this.name=name;
+		this.year=year;}
+	
+}
+const myCar1 = new Car("Ford", 21);
+	const myCar2 = new Car("Good", 23);
+console.log(myCar1.name);
+console.log(myCar2.name);
